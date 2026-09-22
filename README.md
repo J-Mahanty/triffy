de379064ee2b15e9093337e17d2fddc50edecc0c
J-Mahanty <jayatimahanty@gmail.com>
2026-09-22 22:13:20 +0530

    Bot Added to Server, greets people on joining

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ CIT - 01, Analysis of traffic flow and best route prediction
 
 # Running the Discord bot 
 - Join the discord server to start talking to bot using [this link](https://discord.gg/rEpvnmGtRV)
+- The bot will then DM you and you can follow the instructions next to mention your origin and destination of travel.
--- discordBot/main.py
+++ discordBot/main.py
@@ -4,21 +4,36 @@ from dotenv import load_dotenv
 import discord
 import os
 
+
 load_dotenv()
 
 server_bot_token = os.getenv('SERVER_BOT_TOKEN')
-channel_id = os.getenv('CHANNEL_ID')
+channel_id = int (os.getenv('CHANNEL_ID'))
 
 client = commands.Bot(command_prefix= '!', intents=discord.Intents.all())
 
+
 @client.event
-async def on_ready() : 
+async def on_ready(): 
     print("Triffy is now online")
+    try:
+        channel = await client.fetch_channel(channel_id)
+        await channel.send("Triffy is now online")
+    except discord.NotFound:
+        print("Incorrect Channel ID")
+    except discord.Forbidden:
+        print("Triffy Does not have permission to view this")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+@client.event
+async def on_member_join(member) :
     channel = client.get_channel(channel_id)
-    await channel .send("Triffy is now online")
+    await channel.send(f"Welcome to your Traffic Management app {member.mention}! We will dm you shortly with more.")
 
 
-@client.command
+@client.command() 
 async def hello(ctx) :
     await ctx.send(f"Hello {ctx.author.name}, I am triffy")
 
