bb59c84f71fab059b0d01438833c367e56562c4a
J-Mahanty <jayatimahanty@gmail.com>
2026-09-22 21:14:07 +0530

    Adding bot to server

--- discordBot/main.py
+++ discordBot/main.py
@@ -7,12 +7,15 @@ import os
 load_dotenv()
 
 server_bot_token = os.getenv('SERVER_BOT_TOKEN')
+channel_id = os.getenv('CHANNEL_ID')
 
 client = commands.Bot(command_prefix= '!', intents=discord.Intents.all())
 
 @client.event
 async def on_ready() : 
     print("Triffy is now online")
+    channel = client.get_channel(channel_id)
+    await channel .send("Triffy is now online")
 
 
 @client.command
