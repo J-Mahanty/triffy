bea033780611217549f93b6b3a687ea12d361e1b
J-Mahanty <jayatimahanty@gmail.com>
2026-09-25 19:02:57 +0530

    Added Command to accept Origin and Destination from user

--- discordBot/main.py
+++ discordBot/main.py
@@ -1,8 +1,11 @@
 from discord.ext import commands 
 from dotenv import load_dotenv
+from datetime import datetime 
+from zoneinfo import ZoneInfo
 
 import discord
 import os
+import asyncio
 
 
 load_dotenv()
@@ -14,6 +17,7 @@ client = commands.Bot(command_prefix= '!', intents=discord.Intents.all())
 
 
 @client.event
+# On the bot's Startup 
 async def on_ready(): 
     print("Triffy is now online")
     try:
@@ -28,6 +32,7 @@ async def on_ready():
 
 
 @client.event
+# When a new member joins
 async def on_member_join(member) :
     #Send hello greeting in server
     channel = client.get_channel(channel_id)
@@ -37,7 +42,7 @@ async def on_member_join(member) :
     try:
         user = await client.fetch_user(member.id)
         await channel.send(f"Attempting to direct message {member.name}")
-        await user.send(f"Hello {user.name}! This is the start of your chat with Triffy")
+        await user.send(f"Hello {user.name}! This is the start of your chat with Triffy. Say something to get started.")
     except discord.Forbidden:
         # If permission denied
         await channel.send(f"Could not DM {member.name}. They have DMs disabled.")
@@ -46,13 +51,44 @@ async def on_member_join(member) :
 
 
 @client.command(help="Say hello to Triffy") 
-#Reply to hello greeting
+# Reply to hello greeting
 async def hello(ctx) :
     await ctx.send(f"Hello {ctx.author.name}, I am triffy")
 
 
+@client.command(help="Start a new trip")
+# Information about the Origin, Destination and Time of travel. 
+async def route(ctx):
+    
+    await ctx.send("Current Location?")
+
+    # Making sure bot responds in the same channel, with the same user
+    def check(message):
+        return message.author == ctx.author and message.channel == ctx.channel
+
+    try:
+        origin_message = await client.wait_for("message", check=check, timeout=120)
+        origin = origin_message.content
+
+        await ctx.send("Destination?")
+        destination_message = await client.wait_for("message", check=check, timeout=120)
+        destination = destination_message.content
+
+        current_time = datetime.now(ZoneInfo("Asia/Kolkata"))
+
+        await ctx.send(
+            f"**Trip saved!**\n\n"
+            f"**Origin:** {origin}\n"
+            f"**Destination:** {destination}\n"
+            f"**Started at:** {current_time.strftime('%I:%M %p')}"
+        )
+
+    except asyncio.TimeoutError:
+        await ctx.send("You took too long to respond. Please use !route again.")
+
 
 @client.event
+# Error Handling
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         command_list = "\n".join(
