ce3f810947a51e193219bc1ad1f2f53720fe88bf
J-Mahanty <jayatimahanty@gmail.com>
2026-09-25 18:31:10 +0530

    Error messages with the list of commands

--- discordBot/main.py
+++ discordBot/main.py
@@ -36,7 +36,7 @@ async def on_member_join(member) :
     #Send Dm to member
     try:
         user = await client.fetch_user(member.id)
-        await channel.send(f"Attemptig to direct message {member.name}")
+        await channel.send(f"Attempting to direct message {member.name}")
         await user.send(f"Hello {user.name}! This is the start of your chat with Triffy")
     except discord.Forbidden:
         # If permission denied
@@ -45,9 +45,28 @@ async def on_member_join(member) :
         print(f"An error occurred while DMing: {e}")
 
 
-@client.command() 
+@client.command(help="Say hello to Triffy") 
 #Reply to hello greeting
 async def hello(ctx) :
     await ctx.send(f"Hello {ctx.author.name}, I am triffy")
+
+
+
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        command_list = "\n".join(
+            f"`!{command.name}` - {command.help or 'No description available'}"
+            for command in client.commands
+        )
+
+        await ctx.send(
+            "Invalid command!\n\n"
+            "**Available commands:**\n"
+            f"{command_list}"
+        )
+        return 
+    raise error
+
 
 client.run(server_bot_token)
